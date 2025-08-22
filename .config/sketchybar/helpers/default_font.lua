e52662f548f401d1eab0e43057056cb5ff16cdd1
Patrick Byrne <pby.software@byrne.dk>
2025-08-22 17:53:54 +0200

    Update default font to JetBrainsMono Nerd Font

--- .config/sketchybar/helpers/default_font.lua
+++ .config/sketchybar/helpers/default_font.lua
@@ -1,13 +1,8 @@
 return {
-  text = "SF Pro", -- Used for text
-  numbers = "SF Mono", -- Used for numbers
-
-  -- Unified font style map
-  style_map = {
-    ["Regular"] = "Regular",
-    ["Semibold"] = "Semibold",
-    ["Bold"] = "Bold",
-    ["Heavy"] = "Heavy",
-    ["Black"] = "Black",
-  }
+    text = "JetBrainsMono Nerd Font",
+    numbers = "JetBrainsMono Nerd Font",
+    style_map = {
+        ["Regular"] = "Regular",
+        ["Bold"] = "Bold",
+    }
 }
